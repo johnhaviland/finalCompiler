c7b979780b1e4af645a1093f654f255028d9f7e3
johnhaviland <138923412+johnhaviland@users.noreply.github.com>
2023-07-31 13:19:48 -0700

    Update Assembly.h

--- Assembly.h
+++ Assembly.h
@@ -1,9 +1,9 @@
 // Header file to create Assembly code
 #include <stdio.h>
 
-FILE * MIPScode = fopen("MIPScode.asm", "w");
-
 void  initAssemblyFile(){
+    FILE * MIPScode;
+    MIPScode = fopen("MIPScode.asm", "w");
     printf("accessed initAssemblyFile");
     printf("opened MIPScode");
     
@@ -13,27 +13,36 @@ void  initAssemblyFile(){
 }
 
 void emitMIPSAssignment(char * id1, char * id2){
-  fprintf(MIPScode, "li $t1,%s\n", id1);
-  fprintf(MIPScode, "li $t2,%s\n", id2);
-  fprintf(MIPScode, "li $t2,$t1\n\n");
+    FILE * MIPScode;
+    MIPScode = fopen("MIPScode.asm", "w");
+    fprintf(MIPScode, "li $t1,%s\n", id1);
+    fprintf(MIPScode, "li $t2,%s\n", id2);
+    fprintf(MIPScode, "li $t2,$t1\n\n");
 }
 
 void emitMIPSConstantIntAssignment (char id1[50], char id2[50],int currentScope[50]){
+    FILE * MIPScode;
+    MIPScode = fopen("MIPScode.asm", "w");
     fprintf(MIPScode, "li $t%d,%s\n\n", currentScope, id2);
 }
 
 void emitMIPSWriteId(char * id, int count){
-        fprintf(MIPScode, "move $a0,$t%d\n", count);
-        fprintf(MIPScode, "li $v0,1\n");
-        fprintf(MIPScode, "syscall\n");
-        fprintf(MIPScode, "li $a0, 10\nli $v0, 11\nsyscall\n\n");
+    FILE * MIPScode;
+    MIPScode = fopen("MIPScode.asm", "w");
+    fprintf(MIPScode, "move $a0,$t%d\n", count);
+    fprintf(MIPScode, "li $v0,1\n");
+    fprintf(MIPScode, "syscall\n");
+    fprintf(MIPScode, "li $a0, 10\nli $v0, 11\nsyscall\n\n");
 }
 
 void emitEndOfAssemblyCode(){
+    FILE * MIPScode;
+    MIPScode = fopen("MIPScode.asm", "w");
     fprintf(MIPScode, "# ==================================\n\n");
     fprintf(MIPScode, "li $v0,10\n");
     fprintf(MIPScode, "syscall\n");
     fprintf(MIPScode, ".end main\n");
+}
 
     fclose(MIPScode);
 }

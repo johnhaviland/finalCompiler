25de683df59120dbb381ebb185358704dd66e7dc
johnhaviland <138923412+johnhaviland@users.noreply.github.com>
2023-08-02 20:55:13 -0700

    Update MIPScode.asm

--- MIPScode.asm
+++ MIPScode.asm
@@ -1,8 +1,16 @@
 .text
-main:
+.globl main
+main: 
 # ==================================
 
-
+li $t0, 10
+li $t1, 11
+li $t2, 5
+jal print
+print:
+  # print function body
+  # function changed so previous version lost
+  jr $ra
 
 li $v0,10
 

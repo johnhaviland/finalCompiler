ee4340f071a81f6fecd998b30f59aeae0a1cdde7
johnhaviland <138923412+johnhaviland@users.noreply.github.com>
2023-08-05 11:36:00 -0700

    Update IRcode.h

--- IRcode.h
+++ IRcode.h
@@ -5,23 +5,18 @@ FILE * IRcode;
 int numbers[10];
 
 void initIRcodeFile(){
-    FILE * IRcode;
     IRcode = fopen("IRcode.ir", "w");
     fprintf(IRcode, "\n\n#### IR Code ####\n\n");
     fclose(IRcode);
 }
 
 void emitBinaryOperation(char * op[1], const char * id1, const char * id2){
-    FILE * IRcode;
-    IRcode = fopen("IRcode.ir", "a");
     fprintf(IRcode, "T1 = %s %s %s", id1, op, id2);
     fclose(IRcode);
 }
 
 void emitAssignment(char * id1, char * id2){
 
-  FILE * IRcode;
-  IRcode = fopen("IRcode.ir", "a");
   fprintf(IRcode, "T0 = %s\n", id1);
   fprintf(IRcode, "T1 = %s\n", id2);
   fprintf(IRcode, "T1 = T0\n");
@@ -29,9 +24,19 @@ void emitAssignment(char * id1, char * id2){
 }
 
 void emitConstantIntAssignment (char * id1[50], char * id2[50]){
-    FILE * IRcode;
     IRcode = fopen("IRcode.ir", "a");
     fprintf(IRcode, "T%d = %s\n", id2, id1);
+    fclose(IRcode);
+}
+
+void emitIR(char * id1[50], char * id2[50], char * currentScope[50]){
+    IRcode = fopen("IRcode.ir", "a");
+    fprintf(IRcode, "T%d = %s\n", currentScope, id2);
+    fclose(IRcode);
+}
+
+void emitWriteId(char * id){
+    fprintf (IRcode, "output %s\n", "T2");
     fclose(IRcode);
 }
 
@@ -45,20 +50,6 @@ void emitFunctionIR(char * functionName, char * parameters){
     fclose(IRcode);
 }
 
-void emitIR(char * id1[50], char * id2[50], char * currentScope[50]){
-    FILE * IRcode;
-    IRcode = fopen("IRcode.ir", "a");
-    fprintf(IRcode, "T%d = %s\n", currentScope, id2);
-    fclose(IRcode);
-}
-
-void emitWriteId(char * id){
-    FILE * IRcode;
-    IRcode = fopen("IRcode.ir", "a");
-    fprintf (IRcode, "output %s\n", "T2");
-    fclose(IRcode);
-}
-
 void addNumbers(int num){
     int index = 0;
     numbers[index] = num;
@@ -67,7 +58,7 @@ void addNumbers(int num){
 
 int sumOfNumbers(){
     int sum = 0;
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < 50; i++){
         sum += numbers[i];
         numbers[i] = 0;
     }

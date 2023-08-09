e87921e6518d20839d005de0bb59432d6ee4e335
johnhaviland <138923412+johnhaviland@users.noreply.github.com>
2023-08-08 22:03:27 -0700

    Update MIPScode.asm

--- MIPScode.asm
+++ MIPScode.asm
@@ -3,17 +3,23 @@
 main: 
 # ==================================
 
-li $t0, 10
-li $t1, 11
-li $t2, 5
-jal print
-print:
-  # print function body
-  # function changed so previous version lost
-  jr $ra
+li $t0,10
 
-li $v0,10
+li $t1,11
 
+li $t2,5
 
+move $a0,$t4
+li $v0,1
 syscall
-.end main
+li $a0, 10
+li $v0, 11
+syscall
+
+jal print
+
+print:
+	lw $a0, 
+	li $v0, 1
+	syscall
+	
